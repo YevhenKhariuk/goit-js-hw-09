ac39f4f3e03fdbb8d845f7179973d7c9b747b001
YevhenKhariuk <777genya2009@gmail.com>
2023-06-13 20:59:12 +0300

    timer v3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,65 +1,76 @@
 import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const dateTimePicker = document.getElementById('datetime-picker');
-  const startButton = document.querySelector('[data-start]');
-  const daysValue = document.querySelector('[data-days]');
-  const hoursValue = document.querySelector('[data-hours]');
-  const minutesValue = document.querySelector('[data-minutes]');
-  const secondsValue = document.querySelector('[data-seconds]');
+const datetimePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
 
-  let countdownInterval;
-
-  flatpickr(dateTimePicker, {
-    enableTime: true,
-    dateFormat: 'Y-m-d H:i',
-    minDate: 'today',
-    onChange: function (selectedDates) {
-      const selectedDate = selectedDates[0];
-      const currentDate = new Date();
-
-      if (selectedDate > currentDate) {
-        startButton.removeAttribute('disabled');
-      } else {
-        startButton.setAttribute('disabled', 'disabled');
-      }
-    },
-  });
-
-  startButton.addEventListener('click', function () {
-    const selectedDate = new Date(dateTimePicker.value).getTime();
-    const currentDate = new Date().getTime();
-    const timeDifference = selectedDate - currentDate;
-
-    if (timeDifference > 0) {
-      startButton.setAttribute('disabled', 'disabled');
-
-      countdownInterval = setInterval(function () {
-        const currentTime = new Date().getTime();
-        const remainingTime = selectedDate - currentTime;
-
-        if (remainingTime > 0) {
-          const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-          const hours = Math.floor(
-            (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-          );
-          const minutes = Math.floor(
-            (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
-          );
-          const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
-
-          daysValue.textContent = formatTimeValue(days);
-          hoursValue.textContent = formatTimeValue(hours);
-          minutesValue.textContent = formatTimeValue(minutes);
-          secondsValue.textContent = formatTimeValue(seconds);
-        } else {
-          clearInterval(countdownInterval);
-        }
-      }, 1000);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate < new Date()) {
+      window.alert('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
     }
-  });
+  },
+};
 
-  function formatTimeValue(value) {
-    return value < 10 ? `0${value}` : value;
-  }
-});
+flatpickr(datetimePicker, options);
+
+function startCountdown() {
+  const selectedDate = new Date(datetimePicker.value).getTime();
+  const updateTimer = setInterval(() => {
+    const currentDate = new Date().getTime();
+    const difference = selectedDate - currentDate;
+
+    if (difference <= 0) {
+      clearInterval(updateTimer);
+      daysValue.textContent = '00';
+      hoursValue.textContent = '00';
+      minutesValue.textContent = '00';
+      secondsValue.textContent = '00';
+      startButton.disabled = false; // Активуємо кнопку після закінчення відліку
+      datetimePicker.disabled = false; // Активуємо вікно вибору дати після закінчення відліку
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(difference);
+
+    daysValue.textContent = addLeadingZero(days);
+    hoursValue.textContent = addLeadingZero(hours);
+    minutesValue.textContent = addLeadingZero(minutes);
+    secondsValue.textContent = addLeadingZero(seconds);
+  }, 1000);
+
+  startButton.disabled = true; // Деактивуємо кнопку при запуску відліку
+  datetimePicker.disabled = true; // Деактивуємо вікно вибору дати при запуску відліку
+}
+
+startButton.addEventListener('click', startCountdown);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
